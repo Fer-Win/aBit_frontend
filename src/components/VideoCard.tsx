4361b93af200279b603c0cd37ec7781dc5b370ff
Ferwin Lopez <ferwinlopezt@gmail.com>
2024-02-03 20:45:40 +0530

    feat : videoCard component done

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,3 +1,4 @@
+import VideoCardTag from "./VideoCardTag"
 
 
 
@@ -6,13 +7,22 @@ interface VideoCardProps{
     shares : number,
     offered: number,
     raised: number,
-    imgurl: string
+    imgUrl: string
 }
 
 const VideoCard : React.FC<VideoCardProps> = ({title,shares,offered,raised,imgUrl}) => {
   return (
-    <div className="h-36 w-32 bg-blue-200">
-        
+    <div 
+    style={{ backgroundImage: `url(${imgUrl})`, boxShadow : '0px 0px 15px #5c5e5d'}} 
+    className='h-80 w-72 overflow-hidden bg-cover rounded-3xl relative object-cover text-white flex flex-col justify-between '>
+    
+    <div>
+    <div className="font-bold text-[22px] text-left py-5 px-5" >{title}</div>
+    </div>
+    <div className="mb-4">
+    <VideoCardTag shares={shares} offered={offered} raised={raised} ></VideoCardTag>
+    </div>
+    <div className="w-full h-full absolute bg-black bg-opacity-10 top-0"></div>
     </div>
   )
 }
---
+++ src/components/VideoCardTag.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+interface VideoCardTagProps{
+    shares: number,
+    offered: number,
+    raised: number
+}
+const VideoCardTag:React.FC<VideoCardTagProps> = ({shares,offered,raised}) => {
+  return (
+    <div className='px-1 w-[85%] mx-auto rounded-xl text-white bg-white bg-opacity-20 backdrop-blur-2xl flex justify-around py-2'>
+        <div className='flex flex-col items-center '>
+            <p className='font-medium text-sm'>Shares</p>
+            <p className='text-base'>{shares}</p>
+        </div>
+        <div className='flex flex-col items-center'>
+            <p className='font-medium text-sm'>Offered</p>
+            <p className='text-base'>{offered}</p>
+        </div>
+        <div className='flex flex-col items-center'>
+            <p className='font-medium text-sm'>Raised</p>
+            <p className='text-base'>{raised}</p>
+        </div>
+    </div>
+  )
+}
+
+export default VideoCardTag
