2eac25e8ca72288d75c322ece36f32ef6cb49e1f
Ferwin Lopez <ferwinlopezt@gmail.com>
2024-02-05 21:55:11 +0530

    feat : reponsive fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <div className='w-screen h-28 bg-[#DDDEE0] border-b-2 border-black flex justify-between items-center pl-10 pr-4'>
       <div className="text-2xl font-bold ">aBit</div>
-      <div className="hidden md:flex w-1/3 justify-around items-center">
+      <div className="hidden  md:flex w-1/3 justify-around items-center">
         <button className="text-[#764CC2] bg-transparent text-sm font-bold  flex px-6 py-3 border-[3px] rounded-full border-[#764CC2] hover:text-[#DDDEE0] hover:bg-[#764CC2] transition-all duration-200 ease-in-out">Share new video</button>
         <div className="text-[#918c8c] flex justify-around items-center   w-1/2">
         <FaBell size={30}  className="hover:text-black transition-all duration-300 ease-out cursor-pointer"/>
@@ -18,7 +18,7 @@ const Navbar = () => {
         <IoIosArrowDown size={30} className="hover:text-black transition-all duration-300 ease-out cursor-pointer"/>
         </div>
       </div>
-      <div className="flex justify-center items-center gap-5">
+      <div className=" md:hidden flex justify-center items-center gap-5">
       <div className="md:hidden  h-12 w-12 block rounded-full border-2 border-black bg-slate-500 overflow-hidden">
           <img src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
         </div>
--- src/components/Releases.tsx
+++ src/components/Releases.tsx
@@ -2,35 +2,35 @@
 
 const Releases = () => {
   return (
-    <div className='h-[100px] w-[95%] flex-col   md:flex md:flex-row  justify-between'>
+    <div className='h-[100px] w-[95%] flex-col  md:flex md:flex-row  justify-between'>
         <div className='w-full  md:w-1/3 h-full flex flex-col gap-2 justify-center items-center md:items-start '>
-            <h2 className='font-medium text-2xl'>Releases</h2>
-            <p className='text-center text-lg'>Videos that you upload in collaboration with <span className="italic font-bold">aBit</span> appear here.</p>
+            <h2 className='font-medium md:text-xl text-2xl'>Releases</h2>
+            <p className='md:text-left text-center md:text-sm text-lg'>Videos that you upload in collaboration with <span className="italic font-bold">aBit</span> appear here.</p>
         </div>
-        <div className='w-full  md:w-1/3 mt-5 h-[100px] flex    md:flex justify-around'>
-            <div className='w-[25%] h-[80%] my-auto flex  border-l-2 border-[#7A7A7A] '>
+        <div className='w-full md:h-full md:w-2/4 md:mt-0 mt-5 h-[100px] flex md:flex justify-around'>
+            <div className='md:w-1/2 w-[25%] h-[80%] my-auto flex  border-l-2 border-[#7A7A7A] '>
                 <div className='px-2 md:px-4 flex flex-col justify-between'>
                     <h2 className='text-lg font-bold text-[#565656]'>2</h2>
-                    <p className='text-base w-full md:w-2/3 text-[#696969]'>Shared Videos</p>
+                    <p className='md:text-sm text-base w-full md:w-2/3 text-[#696969]'>Shared Videos</p>
                 </div>
             </div>
-            <div className='w-[25%] h-[80%] my-auto flex border-l-2 border-[#7A7A7A] '>
+            <div className='md:w-1/2 w-[25%] h-[80%] my-auto flex border-l-2 border-[#7A7A7A] '>
                 <div className='px-2 md:px-4  flex flex-col justify-between'>
                     <h2 className='text-lg font-bold text-[#565656]'>$9510</h2>
-                    <p className='text-base w-full md:w-2/3 text-[#696969]'>Funds
+                    <p className=' md:text-sm text-base w-full md:w-2/3 text-[#696969]'>Funds
 Raised</p>
                 </div>
             </div>
-            <div className='w-[25%] h-[80%] my-auto flex  border-l-2 border-[#7A7A7A] '>
+            <div className='md:w-1/2 w-[25%] h-[80%] my-auto flex  border-l-2 border-[#7A7A7A] '>
                 <div className='px-2 md:px-4 flex flex-col justify-between'>
                     <h2 className='text-lg font-bold text-[#565656]'>317</h2>
-                    <p className='text-base w-full md:w-2/3 text-[#696969]'>Co-owner community</p>
+                    <p className=' md:text-sm text-base w-full md:w-2/3 text-[#696969]'>Co-owner community</p>
                 </div>
             </div>
-            <div className='w-[25%] h-[80%] my-auto flex  border-l-2 border-[#7A7A7A] '>
+            <div className=' md:w-1/2 w-[25%] h-[80%] my-auto flex  border-l-2 border-[#7A7A7A] '>
                 <div className='px-2 md:px-4 flex flex-col justify-between'>
                     <h2 className='text-lg font-bold text-[#565656]'>$3804</h2>
-                    <p className='text-base w-full md:w-2/3 text-[#696969]'>Co-owner
+                    <p className=' md:text-sm text-base w-full md:w-2/3 text-[#696969]'>Co-owner
 Earnings</p>
                 </div>
             </div>
